cd50f5a1527b7ba21fd67e2833e35cee78f91051
ExM <explorermachine@gmail.com>
2018-06-15 18:52:03 +0300

    wip

--- NMoney/AssemblyInfo.cs
+++ NMoney/AssemblyInfo.cs
@@ -1,13 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NMoney")]
 [assembly: AssemblyDescription("https://github.com/ExM/NMoney")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ExM")]
-[assembly: AssemblyCopyright("MIT Copyright © 2017 Anatoliy Koperin <a.koperin@gmail.com>")]
+[assembly: AssemblyCopyright("MIT Copyright © 2018 Anatoliy Koperin <a.koperin@gmail.com>")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+
