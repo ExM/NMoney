4153ab9e7ff703ea34ea031acf5f95ec44858522
ExM <explorermachine@gmail.com>
2018-06-30 00:45:15 +0300

    wip

--- pack.ps1
+++ pack.ps1
@@ -1,9 +1,3 @@
-$ErrorActionPreference = "Stop"
-$mainFolder = Resolve-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-$paketExe = "$mainFolder\.paket\paket.exe"
-$msbuildExe = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\ToolsVersions\4.0").MSBuildToolsPath -childpath "msbuild.exe"
-
-Remove-Item $mainFolder\Release -recurse -force -ErrorAction 0
-& "$paketExe" restore
-& "$msbuildExe" /m /target:"Clean,Build" /p:Configuration=Release /p:Platform="Any CPU" $mainFolder\NMoney.sln
-& "$paketExe" pack $mainFolder\Release
+& dotnet restore
+& dotnet build -c:Release
+& dotnet pack -c:Release -o:./../Release
