80a5d98ca8204c0914b1c3f1480b26cc0b7ca411
Pavel Laptov <pavel.laptov@ya.ru>
2020-07-08 21:16:35 +0300

    Support MongoDB Bson serialization and query rendering

---
+++ NMoney.Bson.Tests/MongoQueryRenderTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace NMoney.Bson.Tests
+{
+	[TestFixture, Parallelizable(ParallelScope.Children), TestOf(typeof(BsonMoneySerializer))]
+	public class MongoQueryRenderTests
+	{
+		private const string _customAmountFieldName = "sum";
+		private const string _customCurrencyFieldName = "cur";
+
+		private IBsonSerializerRegistry _registry;
+		private IBsonSerializer<Money> _serializer;
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			var registry = new BsonSerializerRegistry();
+			registry.RegisterSerializationProvider(new BsonObjectModelSerializationProvider());
+			BsonCurrencySerializer.Register(Iso4217.CurrencySet.Instance, registry);
+			_serializer = new BsonMoneySerializer(
+				amountFieldName: _customAmountFieldName,
+				currencyFieldName: _customCurrencyFieldName,
+				registry: registry);
+			registry.RegisterSerializer(typeof(Money), _serializer);
+			_registry = registry;
+		}
+
+		[Test]
+		public void RenderEqualityByCurrency()
+		{
+			var filter = Builders<Money>.Filter.Eq(m => m.Currency, Iso4217.CurrencySet.USD);
+			var expected = "{ 'cur' : 'USD' }";
+			var rendered = Render(filter);
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderEqualityByCurrencyExpression()
+		{
+			var filter = Builders<Money>.Filter.Where(m => m.Currency == Iso4217.CurrencySet.USD);
+			var expected = "{ 'cur' : 'USD' }";
+			var rendered = Render(filter);
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderContainsByCurrencyExpression()
+		{
+			var currencies = new[]
+			{
+				Iso4217.CurrencySet.USD,
+				Iso4217.CurrencySet.EUR,
+				Iso4217.CurrencySet.RUB,
+			};
+			var filter = Builders<Money>.Filter.Where(m => currencies.Contains(m.Currency));
+			var expected = "{ 'cur' : { '$in' : ['USD', 'EUR', 'RUB'] } }";
+			var rendered = Render(filter);
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderGreaterThanAmount()
+		{
+			var filter = Builders<Money>.Filter.Where(m => m.Amount > 100);
+			var expected = "{ 'sum' : { '$gt' : NumberDecimal('100') } }";
+			var rendered = Render(filter);
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderIndexDefinition()
+		{
+			var index = Builders<Money>.IndexKeys
+				.Ascending(m => m.Currency)
+				.Ascending(m => m.Amount);
+			var expected = "{ 'cur' : 1, 'sum' : 1 }";
+			var rendered = index.Render(_serializer, _registry).ToJson().Replace('"', '\'');
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderUpdateCurrency()
+		{
+			var update = Builders<Money>.Update
+				.Set(m => m.Currency, Iso4217.CurrencySet.USD);
+			var expected = "{ '$set' : { 'cur' : 'USD' } }";
+			var rendered = update.Render(_serializer, _registry).ToJson().Replace('"', '\'');
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void RenderIncrementAmount()
+		{
+			var update = Builders<Money>.Update
+				.Inc(m => m.Amount, 100m);
+			var expected = "{ '$inc' : { 'sum' : NumberDecimal('100') } }";
+			var rendered = update.Render(_serializer, _registry).ToJson().Replace('"', '\'');
+			Assert.That(rendered, Is.EqualTo(expected));
+		}
+
+		private string Render(FilterDefinition<Money> filter) =>
+			filter
+			.Render(_serializer, _registry)
+			.ToJson()
+			.Replace('"', '\'');
+
+	}
+}
---
+++ NMoney.Bson/BsonMoneySerializer.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace NMoney.Bson
+{
+	/// <summary>
+	/// Bson serializer for <see cref="Money"/>
+	/// </summary>
+	public class BsonMoneySerializer : StructSerializerBase<Money>,
+		IRepresentationConfigurable<BsonMoneySerializer>,
+		IBsonDocumentSerializer
+	{
+		private const string DefaultCurrencyFieldName = "currency";
+		private const string DefaultAmountFieldName = "amount";
+		private const BsonType DefaultAmountRepresentation = BsonType.Decimal128;
+
+		private static readonly object _sync = new object();
+		private static bool _isRegistered = false;
+
+		private readonly string _currencyFieldName;
+		private readonly IBsonSerializer<ICurrency> _currencySerializer;
+		private readonly string _amountFieldName;
+		private readonly DecimalSerializer _amountSerializer;
+
+		/// <summary>
+		/// Initializes a new instance of the BsonMoneySerializer class
+		/// </summary>
+		/// <param name="currencyFieldName">Field name for <see cref="Money.Currency"/> property</param>
+		/// <param name="amountFieldName">Field name for <see cref="Money.Amount"/> property</param>
+		/// <param name="amountRepresentation">Bson representation for <see cref="Money.Amount"/> value</param>
+		/// <param name="registry">Custom serializer registry</param>
+		public BsonMoneySerializer(
+			string currencyFieldName = DefaultCurrencyFieldName,
+			string amountFieldName = DefaultAmountFieldName,
+			BsonType amountRepresentation = DefaultAmountRepresentation,
+			IBsonSerializerRegistry registry = null)
+		{
+			_currencyFieldName = currencyFieldName;
+			_amountFieldName = amountFieldName;
+			Representation = amountRepresentation;
+			_amountSerializer = new DecimalSerializer(Representation);
+			_currencySerializer = (registry ?? BsonSerializer.SerializerRegistry).GetSerializer<ICurrency>();
+		}
+
+		/// <inheritdoc/>
+		public BsonType Representation { get; }
+
+		/// <inheritdoc/>
+		public BsonMoneySerializer WithRepresentation(BsonType representation) =>
+			new BsonMoneySerializer(_currencyFieldName, _amountFieldName, representation);
+
+		/// <inheritdoc/>
+		IBsonSerializer IRepresentationConfigurable.WithRepresentation(BsonType representation) =>
+			WithRepresentation(representation);
+
+		/// <inheritdoc/>
+		public bool TryGetMemberSerializationInfo(string memberName, out BsonSerializationInfo serializationInfo)
+		{
+			switch (memberName)
+			{
+				case nameof(Money.Currency):
+					serializationInfo = new BsonSerializationInfo(
+						_currencyFieldName, _currencySerializer, typeof(ICurrency));
+					return true;
+
+				case nameof(Money.Amount):
+					serializationInfo = new BsonSerializationInfo(
+						_amountFieldName, _amountSerializer, typeof(decimal));
+					return true;
+			}
+
+			serializationInfo = null;
+			return false;
+		}
+
+		/// <inheritdoc/>
+		public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Money value)
+		{
+			var writer = context.Writer;
+
+			if (value == Money.Zero)
+			{
+				writer.WriteNull();
+				return;
+			}
+
+			writer.WriteStartDocument();
+			writer.WriteName(_amountFieldName);
+			_amountSerializer.Serialize(context, args, value.Amount);
+			writer.WriteName(_currencyFieldName);
+			_currencySerializer.Serialize(context, args, value.Currency);
+			writer.WriteEndDocument();
+		}
+
+		/// <inheritdoc/>
+		public override Money Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+		{
+			var reader = context.Reader;
+			var bsonType = reader.GetCurrentBsonType();
+
+			if (bsonType == BsonType.Null)
+			{
+				reader.ReadNull();
+				return Money.Zero;
+			}
+
+			if (bsonType != BsonType.Document)
+				throw new FormatException($"Cannot deserialize BsonString from BsonType {bsonType}.");
+
+			reader.ReadStartDocument();
+
+			decimal amount = default;
+			ICurrency currency = null;
+			while (reader.ReadBsonType() != BsonType.EndOfDocument)
+			{
+				var name = reader.ReadName();
+
+				if (name == _amountFieldName)
+					amount = _amountSerializer.Deserialize(context, args);
+				else if (name == _currencyFieldName)
+					currency = _currencySerializer.Deserialize(context, args);
+				else
+					throw new BsonSerializationException($"Invalid element: '{name}'.");
+			}
+
+			reader.ReadEndDocument();
+
+			if (currency is null)
+				throw new BsonSerializationException($"Document does not contains field {_currencySerializer}");
+
+			return new Money(amount, currency);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="currencyFieldName"></param>
+		/// <param name="amountFieldName"></param>
+		/// <param name="amountRepresentation"></param>
+		public static void Register(
+			string currencyFieldName = DefaultCurrencyFieldName,
+			string amountFieldName = DefaultAmountFieldName,
+			BsonType amountRepresentation = DefaultAmountRepresentation)
+		{
+			if (_isRegistered)
+				return;
+
+			if (!BsonCurrencySerializer.IsRegistered)
+				throw new InvalidOperationException($"{nameof(BsonCurrencySerializer)} must be registered first");
+
+			lock (_sync)
+			{
+				if (_isRegistered)
+					return;
+				_isRegistered = true;
+			}
+
+			BsonSerializer.RegisterSerializer(
+				typeof(Money),
+				new BsonMoneySerializer(currencyFieldName, amountFieldName, amountRepresentation));
+		}
+	}
+}
