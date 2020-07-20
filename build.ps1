ec6a1f8afc9f6f6b9a2e72e8979059c5c77f8468
Anatoliy Koperin <a.koperin@smartcat.ai>
2020-07-20 09:52:57 +0300

    update build scripts

--- build.ps1
+++ build.ps1
@@ -2,5 +2,10 @@ $ErrorActionPreference = "Stop"
 $mainFolder = Resolve-Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 & dotnet clean
+if ($lastexitcode -ne 0) { throw "Error in 'dotnet clean'" }
+
 & dotnet restore
+if ($lastexitcode -ne 0) { throw "Error in 'dotnet restore'" }
+
 & dotnet build -c:Release $mainFolder\NMoney.sln
+if ($lastexitcode -ne 0) { throw "Error in 'dotnet build -c:Release $mainFolder\NMoney.sln'" }
